df2ee10e3a492c27ccc0d2a2e2261d110593bdda
UmerMIB <mohammadumer704@gmail.com>
2019-12-01 12:08:45 +0500

    updated index.js

--- index.js
+++ index.js
@@ -6,26 +6,25 @@ var bodyParser = require('body-parser');
 var app = express.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 process.env.DEBUG = 'dialogflow:*';
-app.post('/webhook', function (request, response) {
+app.post('/webhook', function(request, response) {
     var _agent = new WebhookClient({ request: request, response: response });
     console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
     console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
     function humidity(agent) {
         console.log("context are: ", request.body.queryResult.outputContexts);
         var cityContext = agent.context.get("cityContext");
         var cityName;
         if (agent.parameters.city) {
             cityName = agent.parameters.city;
-        }
-        else if (cityContext.parameters.geo - city) {
+        } else if (cityContext.parameters.geo - city) {
             cityName = cityContext.parameters.geo - city;
-        }
-        else {
+        } else {
             console.log("City name is not provided");
             agent.add("please mention city name ");
         }
         var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey;
-        request(url, function (error, response, body) {
+        request(url, function(error, response, body) {
             if (error) {
                 console.log("Error while calling api");
                 agent.add("Something went wrong while getting the information from External source");
@@ -46,13 +45,18 @@ app.post('/webhook', function (request, response) {
             }));
         });
     }
-    function rain(agent) {
-    }
-    function whether(agent) {
-    }
-    function temperature(agent) {
-    }
-    function wind(agent) {
-    }
+
+    function rain(agent) {}
+
+    function weather(agent) {}
+
+    function temperature(agent) {}
+
+    function wind(agent) {}
     var intentMap = new Map();
+    intentMap.set(`humidity`, humidity);
+    intentMap.set(`rain`, rain);
+    intentMap.set(`weather`, weather);
+    intentMap.set(`temperature`, temperature);
+    intentMap.set(`wind`, wind);
 });
