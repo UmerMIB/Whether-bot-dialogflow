e892f63c2e6400a8f062f2b377e5f6879d7ea413
Umer MiB <mohammadumer704@gmail.com>
2019-12-07 19:09:38 +0500

    make it better

--- index.ts
+++ index.ts
@@ -11,53 +11,96 @@ app.use(bodyParser.urlencoded({ extended: false }))
 process.env.DEBUG = 'dialogflow:*';
 
 app.post('/webhook', function(request,response){
-    const agent = new WebhookClient ({ request, response });
-    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  const agent = new WebhookClient ({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
   
-  function humidity( agent) {
-  //   console.log(`context are: `, request.body.queryResult.outputContexts);
-  //   var cityContext = agent.context.get(`citycontext`);
-  //   var cityName;
-  //   if (agent.parameters.geoCity){
-  //     cityName = agent.parameters.geoCity;
-  //   }else if(cityContext.parameters.geoCity){
-  //     cityName = cityContext.parameters.geoCity;
-  //   }else{
-  //     console.log(`City name is not provided`);
-  //     agent.add(`please mention city name `);
-  //   }
-
-  //   let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
-  //   request(url, (error,response, body )=>{
-  //     if (error){
-  //       console.log(`Error while calling api`);
-  //       agent.add(`Something went wrong while getting the information from External source`);
-  //     }
-  //     let weather  = JSON.parse(body);
-  //     console.log('whether is: \n '+ weather);
-  //     agent.context.set({
-  //       'name':'citycontext',
-  //       'lifespan': 5,
-  //       'parameters':{
-  //         'geoCity':cityName,
-  //         }
-  //     });
-  //     agent.add(new Card ({
-  //         title : `Whether Update`,
-  //         imageURL: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwFfMsIQfjNUeY2QlP7bh9rT2HpXWwHQkRm_pv73oC7AePtidMkA`,
-  //         text : `The humidity in ${cityName} is ${weather.main.humidity}% `
-  //       })
-  //     );
-  //   })
-  }
+ function humidity( agent) {
+    var cityContext = agent.context.get(`citycontext`);
+      console.log('context is ',cityContext)
+      var cityName;
+      if (agent.parameters.geoCity){
+        cityName = agent.parameters['geoCity'];
+      }else if(cityContext.parameters['geoCity']){
+        cityName = cityContext.parameters['geoCity'];
+      }else{
+        console.log(`City name is not provided`);
+        agent.add(`please mention city name `);
+      }
+      return new Promise ((resolved , reject )=>{ 
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+       rq(url, function (error,_response, body ){
+        if (error){
+          console.log(`Error while calling api`);
+          agent.add(`Something went wrong while getting the information from External source`);
+          reject();
+        }
+        let weather  = JSON.parse(body);
+        console.log('whether is: \n ' + weather);
+        agent.context.set({
+          'name':'citycontext',
+          'lifespan': 5,
+          'parameters':{
+            'geoCity':cityName,
+            'geoCity.original':cityName,
+          }
+        });
+        agent.add(new Card ({
+          title : `Humidity Update`,
+          imageUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwFfMsIQfjNUeY2QlP7bh9rT2HpXWwHQkRm_pv73oC7AePtidMkA`,
+          text : `The humidity in ${cityName} is ${weather.main.humidity} `,
+          buttonText: `this is a button`,
+          buttonUrl:`button url`
+          })
+        );
+        resolved();
+      });
+    })
+ }
  function rain(agent){
-
+  var cityContext = agent.context.get(`citycontext`);
+      console.log('context is ',cityContext)
+      var cityName;
+      if (agent.parameters.geoCity){
+        cityName = agent.parameters['geoCity'];
+      }else if(cityContext.parameters['geoCity']){
+        cityName = cityContext.parameters['geoCity'];
+      }else{
+        console.log(`City name is not provided`);
+        agent.add(`please mention city name `);
+      }
+      return new Promise ((resolved , reject )=>{ 
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+       rq(url, function (error,_response, body ){
+        if (error){
+          console.log(`Error while calling api`);
+          agent.add(`Something went wrong while getting the information from External source`);
+          reject();
+        }
+        let weather  = JSON.parse(body);
+        console.log('whether is: \n ' + weather);
+        agent.context.set({
+          'name':'citycontext',
+          'lifespan': 5,
+          'parameters':{
+            'geoCity':cityName,
+            'geoCity.original':cityName,
+          }
+        });
+        agent.add(new Card ({
+          title : `Rain Update`,
+          imageUrl: `http://weather.smh.com.au/styles/icons/fairfax/large/possible_shower.png?1480640737`,
+          text : `The temperature in ${cityName} is ${weather.main.temp} `,
+          buttonText: `this is a button`,
+          buttonUrl:`button url`
+          })
+        );
+        resolved();
+      });
+    })
  }
   
- async function weather(agent){
-  try {
-      console.log(`context are: `, request.body.queryResult.outputContexts);
+ function weather(agent){
       var cityContext = agent.context.get(`citycontext`);
       console.log('context is ',cityContext)
       var cityName;
@@ -69,47 +112,121 @@ app.post('/webhook', function(request,response){
         console.log(`City name is not provided`);
         agent.add(`please mention city name `);
       }
-      return new Promise ((resolved , _reject )=>{ 
+      return new Promise ((resolved , reject )=>{ 
       let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
        rq(url, function (error,_response, body ){
         if (error){
           console.log(`Error while calling api`);
           agent.add(`Something went wrong while getting the information from External source`);
+          reject();
         }
-          let weather  = JSON.parse(body);
-          console.log('whether is: \n ' + weather);
-          
-          agent.context.set({
-              'name':'citycontext',
-              'lifespan': 5,
-              'parameters':{
-                'geoCity':cityName,
-                'geoCity.original':cityName,
-              }
-            });
-              agent.add(new Card ({
-                title : `Whether Update`,
-                imageUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwFfMsIQfjNUeY2QlP7bh9rT2HpXWwHQkRm_pv73oC7AePtidMkA`,
-                text : `The temperature in ${cityName} is ${weather.main.temp}% `,
-                buttonText: `this is a button`,
-                buttonUrl:`button url`
-            })
-          );
-          
-          resolved();
+        let weather  = JSON.parse(body);
+        console.log('whether is: \n ' + weather);
+        agent.context.set({
+          'name':'citycontext',
+          'lifespan': 5,
+          'parameters':{
+            'geoCity':cityName,
+            'geoCity.original':cityName,
+          }
         });
-      })
-    } catch (error) {
-    console.log(`Error occurred in async function : \n`, error)
-  }
+        agent.add(new Card ({
+          title : `Whether Update`,
+          imageUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuqgV7ULWjc32FYJQqOIyrWA-W8NP8qzkjiBkQnD4uVNdsRXziYw`,
+          text : `The weather in ${cityName} is ${weather.weather[0].description} `,
+          buttonText: `this is a button`,
+          buttonUrl:`button url`
+          })
+        );
+        resolved();
+      });
+    })
  }
 
  function temperature(agent){
-
+  var cityContext = agent.context.get(`citycontext`);
+      console.log('context is ',cityContext)
+      var cityName;
+      if (agent.parameters.geoCity){
+        cityName = agent.parameters['geoCity'];
+      }else if(cityContext.parameters['geoCity']){
+        cityName = cityContext.parameters['geoCity'];
+      }else{
+        console.log(`City name is not provided`);
+        agent.add(`please mention city name `);
+      }
+      return new Promise ((resolved , reject )=>{ 
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+       rq(url, function (error,_response, body ){
+        if (error){
+          console.log(`Error while calling api`);
+          agent.add(`Something went wrong while getting the information from External source`);
+          reject();
+        }
+        let weather  = JSON.parse(body);
+        console.log('whether is: \n ' + weather);
+        agent.context.set({
+          'name':'citycontext',
+          'lifespan': 5,
+          'parameters':{
+            'geoCity':cityName,
+            'geoCity.original':cityName,
+          }
+        });
+        agent.add(new Card ({
+          title : `Temperature Update`,
+          imageUrl: `http://weather.smh.com.au/styles/icons/fairfax/large/mostly_sunny.png?1480640735`,
+          text : `The temperature in ${cityName} is ${weather.main.temp} `,
+          buttonText: `this is a button`,
+          buttonUrl:`button url`
+          })
+        );
+        resolved();
+      });
+    })
  }
 
  function wind(agent){
-
+  var cityContext = agent.context.get(`citycontext`);
+      console.log('context is ',cityContext)
+      var cityName;
+      if (agent.parameters.geoCity){
+        cityName = agent.parameters['geoCity'];
+      }else if(cityContext.parameters['geoCity']){
+        cityName = cityContext.parameters['geoCity'];
+      }else{
+        console.log(`City name is not provided`);
+        agent.add(`please mention city name `);
+      }
+      return new Promise ((resolved , reject )=>{ 
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+       rq(url, function (error,_response, body ){
+        if (error){
+          console.log(`Error while calling api`);
+          agent.add(`Something went wrong while getting the information from External source`);
+          reject();
+        }
+        let weather  = JSON.parse(body);
+        console.log('whether is: \n ' + weather);
+        agent.context.set({
+          'name':'citycontext',
+          'lifespan': 5,
+          'parameters':{
+            'geoCity':cityName,
+            'geoCity.original':cityName,
+          }
+        });
+        agent.add(new Card ({
+          title : `Wind Update`,
+          imageUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpItHWtCzBe7XloA1s2uD66uY3iMRNlvNFO5Y66_Pn6VWMc94O`,
+          text : `The wind speed in ${cityName} is ${weather.wind.speed} m/s with ${weather.wind.deg} degrees! `,
+          buttonText: `this is a button`,
+          buttonUrl:`button url`
+          })
+        );
+        resolved();
+      });
+    })
  }
 
 
@@ -123,4 +240,5 @@ app.post('/webhook', function(request,response){
 
  agent.handleRequest(intentMap);
 })
+
 app.listen( port, ()=>{ console.log(`Server is running at ${port}`) } );
